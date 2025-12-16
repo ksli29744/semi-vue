48d26e2d92aed10bf52007679f4eb2ffa8223994
ksli29744 <ksli29744@gmail.com>
2025-12-16 23:01:33 +0800

    semi-rspack-plugin

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,7 +1,8 @@
 import {defineConfig} from '@rsbuild/core'
-import {pluginBabel} from "@rsbuild/plugin-babel"
-import {pluginVue} from "@rsbuild/plugin-vue"
-import {pluginVueJsx} from "@rsbuild/plugin-vue-jsx"
+import {pluginBabel} from '@rsbuild/plugin-babel'
+import {pluginVue} from '@rsbuild/plugin-vue'
+import {pluginVueJsx} from '@rsbuild/plugin-vue-jsx'
+import {SemiRspackPlugin} from '@douyinfe/semi-rspack-plugin'
 
 export default defineConfig({
   source: {
@@ -21,4 +22,14 @@ export default defineConfig({
     pluginVue(),
     pluginVueJsx(),
   ],
+
+  tools: {
+    rspack: { //Rspack 或 webpack 插件
+      plugins: [
+        new SemiRspackPlugin({
+          cssLayer:true
+        })
+      ],
+    },
+  },
 })
